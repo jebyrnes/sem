4fe6d043bb26e9f6ff302cfe76ce372c8c1bfe62
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2018-11-30 15:38:20 -0500

    update make

--- _make.R
+++ _make.R
@@ -1,4 +1,4 @@
 library(bookdown)
 #devtools::install_github("jslefche/piecewiseSEM@devel")
 
-render_book(".", 'bookdown::gitbook', output_dir="docs")
+render_book(".", 'bookdown::gitbook', output_dir="docs", new_session = TRUE)
